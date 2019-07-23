fe3d4831ed01ab7c9ae166271b2a9848829c570d
AnHyun <909833009@qq.com>
2019-07-23 17:01:19 +0800

    罗马数字转换为整数

--- 13.罗马数字转整数.js
+++ 13.罗马数字转整数.js
@@ -8,6 +8,19 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    let sum = 0;
+    let obj = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+    }
+    for(let i = 0; i < s.length; i++) {
+      obj[s[i]] < obj[s[i+1]] ? sum -= obj[s[i]] : sum += obj[s[i]]
+    }
+    return sum;
 };
 
