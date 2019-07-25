cad636fb9486a6e3100a362b8ce4d44a9007507a
AnHyun <909833009@qq.com>
2019-07-25 13:43:38 +0800

    合并两个有序链表

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -16,6 +16,19 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-
+  let rear = new ListNode();
+  let current_reference = rear;
+  while (l1 && l2) {
+    if(l1.val <= l2.val){
+        current_reference.next = new ListNode(l1.val);
+          l1 = l1.next;
+      } else {
+        current_reference.next = new ListNode(l2.val);
+        l2 = l2.next;
+      }
+      current_reference = current_reference.next;
+  }
+  current_reference.next = l1 || l2;
+  return rear.next; 
 };
 
