aa4a4cb60f5eceb56bed15e2054a9dd5e24da4ba
AnHyun <909833009@qq.com>
2019-07-26 09:32:03 +0800

    最后一个单词的长度

---
+++ 58.最后一个单词的长度.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=javascript
+ *
+ * [58] 最后一个单词的长度
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    if (!s) return 0;
+
+    let arr = s.trimRight().split(' ');
+    return arr[arr.length - 1].length
+
+};
+
