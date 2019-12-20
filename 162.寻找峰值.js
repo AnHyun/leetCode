af2b63841feb64ebd06f52aa045fed3614d2d394
AnHyun <909833009@qq.com>
2019-12-20 16:40:54 +0800

    162.寻找峰值

---
+++ 162.寻找峰值.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=javascript
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    // let maxNum = Math.max(...nums)
+    // return nums.indexOf(maxNum)
+
+    let first = nums[0]
+    let hash = {first: 0}
+
+};
+// @lc code=end
+
