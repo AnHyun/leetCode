f8e6e882b99c38730d92e6b86238a327eed349fc
AnHyun <909833009@qq.com>
2019-12-23 17:14:37 +0800

    162.寻找峰值

--- 162.寻找峰值.js
+++ 162.寻找峰值.js
@@ -10,8 +10,19 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let maxNum = Math.max(...nums)
-    return nums.indexOf(maxNum)
+    // let maxNum = Math.max(...nums)
+    // return nums.indexOf(maxNum)
+
+    let left = 0, right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
 
 };
 // @lc code=end
